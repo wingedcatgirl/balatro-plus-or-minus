fcebf6c0eeefd0de04e2c0a13f19245514eaca90
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-03-13 22:06:40 -0400

    grooooan

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -10,47 +10,47 @@ return {
             j_plusminus_velocity = {
                 name = "Velocity Joker",
                 text = {
-                    "{C:chips}+#1#{} Chips per round",
-                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive})",
+                    "{C:chips}+#2#{} Chips per round",
+                    "{C:inactive}(Currently {C:chips}+#1#{C:inactive})",
                 }
             },
             j_plusminus_acceleration = {
                 name = "Acceleration Joker",
                 text = {
-                    "{C:chips}+#1#{} Chips per round, per round",
-                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive})",
+                    "{C:chips}+#2#{} Chips per round, per round",
+                    "{C:inactive}(Currently {C:chips}+#1#{C:inactive})",
                 }
             },
             j_plusminus_jerk = {
                 name = "Jerk Joker",
                 text = {
-                    "{C:chips}+#1#{} Chips per round,",
+                    "{C:chips}+#2#{} Chips per round,",
                     "per round, per round",
-                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive})",
+                    "{C:inactive}(Currently {C:chips}+#1#{C:inactive})",
                 }
             },
             j_plusminus_snap = {
                 name = "Snap Joker",
                 text = {
-                    "{C:chips}+#1#{} Chips per round, per round,",
+                    "{C:chips}+#2#{} Chips per round, per round,",
                     "per round, per round",
-                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive})",
+                    "{C:inactive}(Currently {C:chips}+#1#{C:inactive})",
                 }
             },
             j_plusminus_crackle = {
                 name = "Crackle Joker",
                 text = {
-                    "{C:chips}+#1#{} Chips per round, per round,",
+                    "{C:chips}+#2#{} Chips per round, per round,",
                     "per round, per round, per round",
-                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive})",
+                    "{C:inactive}(Currently {C:chips}+#1#{C:inactive})",
                 }
             },
             j_plusminus_pop = {
                 name = "Pop Joker",
                 text = {
-                    "{C:chips}+#1#{} Chips per round, per round, per round,",
+                    "{C:chips}+#2#{} Chips per round, per round, per round,",
                     "per round, per round, per round",
-                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive})",
+                    "{C:inactive}(Currently {C:chips}+#1#{C:inactive})",
                 }
             },
         }
